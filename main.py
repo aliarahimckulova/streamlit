3eeced36ec2af4b32776817b48c6b9ae800c8bee
Rakhimkulova Aliia <rahimckulova.alia@yandex.ru>
2023-04-28 03:10:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ conn = connect(credentials=credentials)
 def run_query(query):
     rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
-    return rows
+    return tuple(rows)
 
 sheet_url = st.secrets["private_gsheets_url"]
 rows = run_query(f'SELECT * FROM "{sheet_url}"')
@@ -27,4 +27,5 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 # Print results.
 for row in rows:
     st.write(f"{row.name} has a :{row.pet}:")
+
     
